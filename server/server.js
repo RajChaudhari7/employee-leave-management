54b1f543ead91600deaf59d650cb060e923f958e
Raj Chaudhari <rajchaudharii1403@gmail.com>
2026-07-28 12:48:29 +0530

    Started the project started first with the backend completed database and creation of the tables and connection

---
+++ server/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import morgan from "morgan";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(helmet());
+app.use(morgan("dev"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Employee Leave Management Api Running",
+  });
+});
