1bf8f94895948f1237abe7f1d00efb9cf40de557
Raj Chaudhari <rajchaudharii1403@gmail.com>
2026-07-29 13:25:11 +0530

    completed auth middleware role middleware and started with user registeration and login controllers

---
+++ server/src/middleware/auth.middleware.js
@@ -0,0 +1,23 @@
+import { verifyToken } from "../utils/jwt.js";
+
+const authMiddleware = (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader?.startsWith("Bearer")) {
+      return res.status(401).json({ success: false, message: "Unauthorized" });
+    }
+
+    const token = authHeader.split(" ")[1];
+    const decoded = verifyToken(token);
+    req.user = decoded;
+
+    next();
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ success: false, message: "Invalid or Expired Token" });
+  }
+};
+
+export default authMiddleware;
