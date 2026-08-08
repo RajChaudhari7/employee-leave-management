4129c9aac24b7161e13a3b8b7866c4a8b747e8af
Raj Chaudhari <rajchaudharii1403@gmail.com>
2026-08-08 13:39:30 +0530

    updated axios and leave service

--- client/src/services/leave.service.js
+++ client/src/services/leave.service.js
@@ -1,11 +1,7 @@
 import api from "../api/axios";
 
 export const applyLeave = async (formData) => {
-  const response = await api.post("/employee/leave", formData, {
-    headers: {
-      "Content-Type": "multipart/form-data",
-    },
-  });
+  const response = await api.post("/employee/leave", formData);
 
   return response.data;
 };
