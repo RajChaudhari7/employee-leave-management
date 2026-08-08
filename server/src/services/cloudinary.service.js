02e7db90c85cbb0cdf53b13176383034343756e3
Raj Chaudhari <rajchaudharii1403@gmail.com>
2026-08-08 14:22:59 +0530

    fixing the cloudinary error

--- server/src/services/cloudinary.service.js
+++ server/src/services/cloudinary.service.js
@@ -2,18 +2,22 @@ import cloudinary from "../config/cloudinary.js";
 
 export const uploadToCloudinary = (fileBuffer, originalName) => {
   return new Promise((resolve, reject) => {
+    const fileName = originalName
+      .replace(/\.[^/.]+$/, "")
+      .replace(/[^a-zA-Z0-9-_]/g, "-");
+
     const uploadStream = cloudinary.uploader.upload_stream(
       {
         folder: "employee-leave-management/leave-documents",
         resource_type: "auto",
-        public_id: `${Date.now()}-${originalName
-          .replace(/\.[^/.]+$/, "")
-          .replace(/[^a-zA-Z0-9-_]/g, "-")}`,
+        public_id: `${Date.now()}-${fileName}`,
       },
       (error, result) => {
         if (error) {
+          console.error("CLOUDINARY ERROR:", error);
           reject(error);
         } else {
+          console.log("CLOUDINARY UPLOAD SUCCESS:", result.secure_url);
           resolve(result);
         }
       },
